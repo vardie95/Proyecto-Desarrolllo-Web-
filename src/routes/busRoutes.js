49a7fb6ee42abaf9f1bb2f1c0292cc6cac0f16f7
MaoEl26 <m.c.v.2695@gmail.com>
2019-10-27 16:24:46 -0600

    Modificación Datos

--- src/routes/busRoutes.js
+++ src/routes/busRoutes.js
@@ -10,4 +10,17 @@ router.get('/RegisterRoute', (req, res, next) => {
 router.get('/ModifyRoute', (req, res, next) => {
     res.render('busRoutes/modifyRoute');
   });
+
+  router.post('/registerRoute',async(req,res,next)=>{
+    var {inputName, inputNumTelefono, inputEmail, inputZonaOrigen,
+    inputZonaDestino,inputDireccionSenna,inputDiasSemana,inputHoraApertura,
+  inputHoraCierre,inputContactoAnomalias} = req.body;
+  
+  let insertQuery = 'INSERT INTO `RXWuaQvtL6`.`Route`(`numroute`, `description`, `ticketCost`, `durationtime`, `disability`, `frecuency`,'+
+  '`latitude`,`longitude`,`starttime`,`finishtime`,`idCompany`)'+'VALUES (?,?,?,?,?,?,?,?,?,?,?)';
+    let selectQuery = 'Select username from `RXWuaQvtL6`.`Users` where username = ?';
+  
+    
+  
+  });
 module.exports = router;
