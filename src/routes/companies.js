ea7b7dfd65179cd8c1f4b4d9eac4650ee6752ec7
vardie95 <vardie95@gmail.com>
2019-10-27 17:43:09 -0600

    Add point to map

--- src/routes/companies.js
+++ src/routes/companies.js
@@ -15,12 +15,12 @@ router.post('/registerCompany',async(req,res,next)=>{
   var {inputName, inputNumTelefono, inputEmail, inputZonaOrigen,
   inputZonaDestino,inputDireccionSenna,inputDiasSemana,inputHoraApertura,
 inputHoraCierre,inputContactoAnomalias} = req.body;
+    
 
 let insertQuery = 'INSERT INTO `RXWuaQvtL6`.`Company`(`name`, `phone`, `sourcezone`, `destinyzone`, `email`, `anomalycontact`,'+
 '`addresssigns`,`latitude`,`longitude`,`daysattention`,`openingtime`,`closingtime`)'+'VALUES (?,?,?,?,?,?,?,?,?,?,?,?)';
 	let selectQuery = 'Select username from `RXWuaQvtL6`.`Users` where username = ?';
 
-	
 
 });
 module.exports = router;
